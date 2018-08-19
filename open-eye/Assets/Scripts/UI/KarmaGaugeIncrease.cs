d122d743d5328ddf323a16de54b39f02b3895ded
김태영 <u.tokyo0319@gmail.com>
2018-08-19 19:57:26 +0900

    다음 턴, 다음 스폰 업보량 표기, 근데 실행 시 안보이는데 이유를 잘 모르겠음.

---
+++ open-eye/Assets/Scripts/UI/KarmaGaugeIncrease.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KarmaGaugeIncrease : MonoBehaviour {
+
+    [HideInInspector]
+    public float pivotGuage;
+
+    [HideInInspector]
+    public float NextTurnpivotGuage;
+
+    [HideInInspector]
+    public float NextEnemySpawnPivotGuage;
+    private int HowManySpawned = 0;
+
+    public void ChangeKarmaGauge()
+    {
+        pivotGuage = (float)GameManager.instance.karma / GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas
+        [GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas.Count - 1].requiredKarma;
+
+        NextTurnpivotGuage = GameManager.instance.karma + (float)GameManager.instance.notoriety / GameManager.instance
+        .config.enemySpawnDataContainer.enemySpawnDatas[GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas.Count - 1].requiredKarma;
+        GetComponent<RectTransform>().localPosition = new Vector3 (0f, 300.5f - 552f + 552f * pivotGuage, 0f);
+
+        if(GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas[HowManySpawned].requiredKarma < GameManager.instance.karma)
+        {
+            HowManySpawned++;
+        }
+        NextEnemySpawnPivotGuage = (float)GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas
+        [HowManySpawned].requiredKarma / GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas
+        [GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas.Count - 1].requiredKarma;
+
+        GameObject.Find("NextTurnKarma").GetComponent<RectTransform>().localPosition = new Vector3(0f, 552f * NextTurnpivotGuage - 552f, 0f);
+        GameObject.Find("NextEnemySpawnKarma").GetComponent<RectTransform>().localPosition = new Vector3(0f, 552f * NextEnemySpawnPivotGuage - 552f, 0f);
+    }
+}
--- open-eye/Assets/Scripts/UI/NotorietyColorChange.cs
+++ open-eye/Assets/Scripts/UI/NotorietyColorChange.cs
@@ -8,6 +8,6 @@ public class NotorietyColorChange : MonoBehaviour {
     {
         GetComponent<Image>().color = new Color(Mathf.Lerp((float)0xA7 / 255, (float)0x79 / 255, GameObject.Find("KarmaGaugeMid").GetComponent<KarmaGaugeIncrease>().pivotGuage),
            Mathf.Lerp((float)0xCA / 255, (float)0x39 / 255, GameObject.Find("KarmaGaugeMid").GetComponent<KarmaGaugeIncrease>().pivotGuage),
-           Mathf.Lerp((float)0xE9 / 255, (float)0x93 / 255, GameObject.Find("KarmaGaugeMid").GetComponent<KarmaGaugeIncrease>().pivotGuage));
+           Mathf.Lerp((float)0xE9 / 255, (float)0x93 / 255, GameObject.Find("KarmaGaugeMid").GetComponent<KarmaGaugeIncrease>().pivotGuage)); // 게이지 색이 가장 어두워지는 악명의 수치에 대한 논의가 필요할듯. 일단 업보를 의미하는 피봇게이지로 넣어둠(?)
     }
 }
