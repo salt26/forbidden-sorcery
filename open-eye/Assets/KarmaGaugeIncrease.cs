89b35f39b5c935c658edb1663fe82439dbb790a5
김태영 <u.tokyo0319@gmail.com>
2018-08-19 19:26:13 +0900

    업보 게이지가 움직입니다.

--- open-eye/Assets/KarmaGaugeIncrease.cs
+++ open-eye/Assets/KarmaGaugeIncrease.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class KarmaGaugeIncrease : MonoBehaviour {
 
     [HideInInspector]
-    public float pivotGuage = 0.5f; /*(float)GameManager.instance.karma / GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas
-        [GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas.Count - 1].requiredKarma;*/
+    public float pivotGuage;
 
     public void ChangeKarmaGauge()
     {
+        pivotGuage = (float)GameManager.instance.karma / GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas
+       [GameManager.instance.config.enemySpawnDataContainer.enemySpawnDatas.Count - 1].requiredKarma;
         GetComponent<RectTransform>().localPosition = new Vector3 (0f, 300.5f - 552f + 552f * pivotGuage, 0f); 
     }
+
 }
