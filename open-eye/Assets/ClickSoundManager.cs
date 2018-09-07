8c0ca2104d41aea6b994af262b6bc4dbd6943f0e
김태영 <u.tokyo0319@gmail.com>
2018-09-07 16:28:52 +0900

    프로듀스 버튼을 클릭하면 소리가 납니다.

---
+++ open-eye/Assets/ClickSoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSoundManager : MonoBehaviour
+{
+    public AudioClip soundClick; 
+    AudioSource myAudio;
+    public static ClickSoundManager instance; 
+    void Awake()
+    {
+        if (ClickSoundManager.instance == null) 
+        {
+            ClickSoundManager.instance = this;
+        }
+    }
+    void Start()
+    {
+        myAudio = this.gameObject.GetComponent<AudioSource>();
+    }
+    public void PlaySound()
+    {
+        myAudio.PlayOneShot(soundClick);
+    }
+}
