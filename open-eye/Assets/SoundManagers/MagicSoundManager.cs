dc80b7e4aa85fa4739398d6530c58e8c5d7e22fc
김태영 <u.tokyo0319@gmail.com>
2018-09-08 02:26:54 +0900

    전투 소리 추가됨

---
+++ open-eye/Assets/SoundManagers/MagicSoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicSoundManager : MonoBehaviour
+{
+
+    public AudioClip soundClick;
+    AudioSource myAudio;
+
+    public static MagicSoundManager instance;
+
+    void Awake()
+    {
+        if (MagicSoundManager.instance == null)
+        {
+            MagicSoundManager.instance = this;
+        }
+    }
+    void Start()
+    {
+        myAudio = this.gameObject.GetComponent<AudioSource>();
+    }
+    public void PlaySound()
+    {
+        myAudio.PlayOneShot(soundClick);
+    }
+}
