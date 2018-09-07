6b3293685d1cf44b080abba982bd67bdcad0e62a
Coffeetea <coffeetea99iro@gmail.com>
2018-09-08 05:00:15 +0900

    HowToPlay 코드까지 추가

---
+++ open-eye/Assets/Scripts/HowToPlayScene/WholeScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WholeScreen : MonoBehaviour {
+
+    int screenShotNumber = 0;
+
+	public void ScreenBeingClicked()
+    {
+        if (screenShotNumber > 0)
+        {
+            GameObject.Find(screenShotNumber.ToString()).GetComponent<Image>().enabled = false;
+        }
+        ++screenShotNumber;
+        GameObject.Find(screenShotNumber.ToString()).GetComponent<Image>().enabled = true;
+    }
+    
+}
